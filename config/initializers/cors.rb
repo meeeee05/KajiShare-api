78c4c60a6a2facd70412ddfc8d49bdc5437c1898
meeeee05 <stmi.eeeee05@gmail.com>
2025-10-29 23:57:17 +0900

    CORS設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'  # Next.jsのURL
+    origins 'http://localhost:3001'  # Next.js のURLを許可
+
     resource '*',
       headers: :any,
-      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      expose: ['access-token', 'client', 'uid'],  # トークンをフロント側で受け取る
+            methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      get 'users', to: 'users#index'
-    end
-  end
+  mount_devise_token_auth_for 'User', at: 'auth'
 end
